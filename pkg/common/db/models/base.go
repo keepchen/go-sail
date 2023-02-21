5f4a2604497d0a146a7fa8dbde229ef72cb2ddd1
keepchen <keepchen2016@qq.com>
2023-02-21 19:21:54 +0800

    db类库新增分页器和自动同步方法

--- pkg/common/db/models/base.go
+++ pkg/common/db/models/base.go
@@ -2,9 +2,11 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
-//BaseModel 基础模型字段
+// BaseModel 基础模型字段
 type BaseModel struct {
 	ID        uint64     `gorm:"column:id;primary_key; AUTO_INCREMENT"`
 	CreatedAt time.Time  `gorm:"column:created_at;comment:创建时间"`    //创建时间
@@ -12,5 +14,26 @@ type BaseModel struct {
 	DeletedAt *time.Time `gorm:"column:deleted_at;comment:(软)删除时间"` //(软)删除时间
 }
 
-//NoneID 空ID
+// NoneID 空ID
 const NoneID = uint64(0)
+
+// AutoMigrate 自动同步表结构
+func AutoMigrate(db *gorm.DB, tables ...interface{}) {
+	err := db.AutoMigrate(tables...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Paginate 分页器
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page < 1 {
+			page = 0
+		} else {
+			page--
+		}
+
+		return db.Offset(page * pageSize).Limit(pageSize)
+	}
+}
