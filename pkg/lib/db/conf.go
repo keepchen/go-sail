22194baa6a26f5ee7240b3d1da0f0c2594435cc7
keepchen <keepchen2016@qq.com>
2023-02-21 15:25:45 +0800

    fix:db conf tag

--- pkg/lib/db/conf.go
+++ pkg/lib/db/conf.go
@@ -1,8 +1,8 @@
 package db
 
-//Conf 配置信息
+// Conf 配置信息
 type Conf struct {
-	DriverName     string             `yaml:"diver_name" toml:"diver_name" json:"diver_name" default:"mysql"`       //数据库类型
+	DriverName     string             `yaml:"driver_name" toml:"driver_name" json:"driver_name" default:"mysql"`    //数据库类型
 	AutoMigrate    bool               `yaml:"auto_migrate" toml:"auto_migrate" json:"auto_migrate" default:"false"` //是否自动同步表结构
 	LogLevel       string             `yaml:"log_level" toml:"log_level" json:"log_level" default:"info"`           //日志级别
 	ConnectionPool ConnectionPoolConf `yaml:"connection_pool" toml:"connection_pool" json:"connection_pool"`        //连接池配置
@@ -13,7 +13,7 @@ type Conf struct {
 	Clickhouse     ClickhouseConf     `yaml:"clickhouse" toml:"clickhouse" json:"clickhouse"`                       //clickhouse配置
 }
 
-//ConnectionPoolConf 连接池配置
+// ConnectionPoolConf 连接池配置
 type ConnectionPoolConf struct {
 	MaxOpenConnCount       int `yaml:"max_open_conn_count" toml:"max_open_conn_count" json:"max_open_conn_count" default:"100"`                     //最大开启连接数
 	MaxIdleConnCount       int `yaml:"max_idle_conn_count" toml:"max_idle_conn_count" json:"max_idle_conn_count" default:"10"`                      //最大闲置数量
@@ -26,7 +26,7 @@ type MysqlConf struct {
 	Write MysqlConfItem `yaml:"write" toml:"write" json:"write" default:"localhost"` //写实例
 }
 
-//MysqlConfItem mysql配置
+// MysqlConfItem mysql配置
 type MysqlConfItem struct {
 	Host      string `yaml:"host" toml:"host" json:"host" default:"localhost"`           //主机地址
 	Port      int    `yaml:"port" toml:"port" json:"port" default:"3306"`                //端口
@@ -43,7 +43,7 @@ type PostgresConf struct {
 	Write PostgresConfItem `yaml:"write" toml:"write" json:"write" default:"localhost"` //写实例
 }
 
-//PostgresConfItem postgres配置
+// PostgresConfItem postgres配置
 type PostgresConfItem struct {
 	Host     string `yaml:"host" toml:"host" json:"host" default:"localhost"`                 //主机地址
 	Port     int    `yaml:"port" toml:"port" json:"port" default:"9920"`                      //端口
@@ -59,7 +59,7 @@ type SqlserverConf struct {
 	Write SqlserverConfItem `yaml:"write" toml:"write" json:"write" default:"localhost"` //写实例
 }
 
-//SqlserverConfItem sqlserver配置
+// SqlserverConfItem sqlserver配置
 type SqlserverConfItem struct {
 	Host     string `yaml:"host" toml:"host" json:"host" default:"localhost"` //主机地址
 	Port     int    `yaml:"port" toml:"port" json:"port" default:"9930"`      //端口
@@ -73,7 +73,7 @@ type SqliteConf struct {
 	Write SqliteConfItem `yaml:"write" toml:"write" json:"write" default:"localhost"` //写实例
 }
 
-//SqliteConfItem sqlite配置
+// SqliteConfItem sqlite配置
 type SqliteConfItem struct {
 	File string `yaml:"file" toml:"file" json:"file" default:"sqlite.db"` //数据库文件
 }
@@ -83,7 +83,7 @@ type ClickhouseConf struct {
 	Write ClickhouseConfItem `yaml:"write" toml:"write" json:"write" default:"localhost"` //写实例
 }
 
-//ClickhouseConfItem clickhouse配置
+// ClickhouseConfItem clickhouse配置
 type ClickhouseConfItem struct {
 	Host         string `yaml:"host" toml:"host" json:"host" default:"localhost"`                     //主机地址
 	Port         int    `yaml:"port" toml:"port" json:"port" default:"9000"`                          //端口
