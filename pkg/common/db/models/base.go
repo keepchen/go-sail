a4af5e344b5d45d679f027c1d3ce550d5b51479c
keepchen <keepchen2016@qq.com>
2023-02-28 14:00:31 +0800

    分页器参数默认值

--- pkg/common/db/models/base.go
+++ pkg/common/db/models/base.go
@@ -26,12 +26,18 @@ func AutoMigrate(db *gorm.DB, tables ...interface{}) {
 }
 
 // Paginate 分页器
+//
+// page默认为0，pageSize默认为10
 func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if page < 1 {
 			page = 0
 		} else {
 			page--
+		}
+
+		if pageSize < 1 {
+			pageSize = 10
 		}
 
 		return db.Offset(page * pageSize).Limit(pageSize)
