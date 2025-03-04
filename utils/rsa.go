3f31b32edd5aec04a128150101f135a1539cd91c
keepchen <keepchen2016@qq.com>
2025-03-04 14:08:27 +0800

    utils:rsa新增格式化函数兼容多种格式的公私钥

--- utils/rsa.go
+++ utils/rsa.go
@@ -8,6 +8,10 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/keepchen/go-sail/v3/constants"
 )
 
 type rsaImpl struct {
@@ -15,12 +19,36 @@ type rsaImpl struct {
 
 type IRsa interface {
 	// Encrypt rsa加密
+	//
+	// publicKey格式支持：
+	//
+	// - 标准格式
+	//
+	// - 不带前后缀的一行字符串
 	Encrypt(rawString string, publicKey []byte) (string, error)
 	// Decrypt rsa解密
+	//
+	// privateKey格式支持：
+	//
+	// - 标准格式
+	//
+	// - 不带前后缀的一行字符串
 	Decrypt(encodedString string, privateKey []byte) (string, error)
 	// Sign rsa加签
+	//
+	// privateKey格式支持：
+	//
+	// - 标准格式
+	//
+	// - 不带前后缀的一行字符串
 	Sign(rawStringBytes, privateKey []byte) (string, error)
 	// VerifySign rsa验签
+	//
+	// publicKey格式支持：
+	//
+	// - 标准格式
+	//
+	// - 不带前后缀的一行字符串
 	VerifySign(rawStringBytes, sign, publicKey []byte) (bool, error)
 }
 
@@ -32,6 +60,9 @@ func RSA() IRsa {
 // Encrypt rsa加密
 func (rsaImpl) Encrypt(rawString string, publicKey []byte) (string, error) {
 	pubObj := parsePublicKey(publicKey)
+	if pubObj == nil {
+		return "", errors.New("public key is nil")
+	}
 
 	encodedByte, err := rsa.EncryptPKCS1v15(rand.Reader, pubObj, []byte(rawString))
 	if err != nil {
@@ -49,6 +80,9 @@ func (rsaImpl) Decrypt(encodedString string, privateKey []byte) (string, error) 
 	}
 
 	priObj := parsePrivateKey(privateKey)
+	if priObj == nil {
+		return "", errors.New("private key is nil")
+	}
 	decodedByte, err := rsa.DecryptPKCS1v15(rand.Reader, priObj, encodedByte)
 	if err != nil {
 		return "", err
@@ -67,6 +101,9 @@ func (rsaImpl) Sign(rawStringBytes, privateKey []byte) (string, error) {
 	d := h.Sum(nil)
 
 	priObj := parsePrivateKey(privateKey)
+	if priObj == nil {
+		return "", errors.New("private key is nil")
+	}
 
 	sign, err := rsa.SignPKCS1v15(rand.Reader, priObj, crypto.SHA256, d)
 
@@ -83,6 +120,9 @@ func (rsaImpl) VerifySign(rawStringBytes, sign, publicKey []byte) (bool, error) 
 	d := h.Sum(nil)
 
 	pubObj := parsePublicKey(publicKey)
+	if pubObj == nil {
+		return false, errors.New("public key is nil")
+	}
 
 	decodedSign, err := Base64().Decode(string(sign))
 	if err != nil {
@@ -96,6 +136,7 @@ func (rsaImpl) VerifySign(rawStringBytes, sign, publicKey []byte) (bool, error) 
 
 // 解析公钥
 func parsePublicKey(key []byte) *rsa.PublicKey {
+	key = formatKey(key, "public")
 	block, _ := pem.Decode(key)
 
 	if block == nil {
@@ -113,6 +154,7 @@ func parsePublicKey(key []byte) *rsa.PublicKey {
 
 // 解析私钥
 func parsePrivateKey(key []byte) *rsa.PrivateKey {
+	key = formatKey(key, "private")
 	block, _ := pem.Decode(key)
 	if block == nil {
 		return nil
@@ -125,4 +167,64 @@ func parsePrivateKey(key []byte) *rsa.PrivateKey {
 	priObj := pri.(*rsa.PrivateKey)
 
 	return priObj
+}
+
+// 格式化公私钥
+//
+// 支持格式：
+//
+// - 标准格式
+//
+// -----BEGIN PRIVATE KEY-----
+//
+// MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUvUDx+LPQ0S+L
+//
+// ...
+//
+// xUK2duHjDxiaPKtANi2P4
+//
+// -----END PRIVATE KEY-----
+//
+// - 非标准格式：一行不带前缀或不带后缀
+//
+// xMIIEvgI...UK2duHjDxiaPKtANi2P4
+//
+// - 非标准格式：一行带前缀或后缀
+//
+// -----BEGIN PRIVATE KEY-----MIIEvgI...UK2duHjDxiaPKtANi2P4-----END PRIVATE KEY-----
+func formatKey(key []byte, typ string) []byte {
+	var (
+		prefix string
+		suffix string
+		keyStr = string(key)
+	)
+	if typ == "public" {
+		prefix = constants.PublicKeyBeginStr
+		suffix = constants.PublicKeyEndStr
+	} else {
+		prefix = constants.PrivateKeyBeginStr
+		suffix = constants.PrivateKeyEndStr
+	}
+	//按每行64个字符拆分
+	if !strings.Contains(keyStr, "\n") {
+		//存在前缀，先去掉
+		if strings.HasPrefix(keyStr, prefix) {
+			keyStr = strings.Replace(keyStr, prefix, "", 1)
+		}
+		//存在后缀，先去掉
+		if strings.HasSuffix(keyStr, suffix) {
+			keyStr = strings.Replace(keyStr, suffix, "", 1)
+		}
+		keyStr = String().Wordwrap(keyStr, 64, "\n")
+	}
+	//检测前缀，没有则添加上
+	if !strings.HasPrefix(keyStr, prefix) {
+		keyStr = fmt.Sprintf("%s\n%s", prefix, keyStr)
+	}
+	//检测后缀，没有则添加上
+	if !strings.HasSuffix(keyStr, suffix) {
+		keyStr = fmt.Sprintf("%s\n%s", keyStr, suffix)
+	}
+
+	return []byte(keyStr)
 }
--- utils/rsa_test.go
+++ utils/rsa_test.go
@@ -45,10 +45,18 @@ u/Wq722YIEH39RpL2ydxTP2q5bBNrLeDWdxyBxZlP9zZ/T7AW+boqIGDdPHKBmWr
 KTJQ+GGzUqOGzruYQ5sM3TnU8Avb4OF36uyADBwA4bP944tKSNSET7BC3N0UerRo
 QwIDAQAB
 -----END PUBLIC KEY-----`)
+
+	privateKeyInline = []byte(`MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUvUDx+LPQ0S+L+5UmtD2EJw1L953mVCMWBJktBbqPTIhDmrd33+3cNq0t7rXuALhoqZS/53nDchU1wsCveieNDR7SsdO4HMS4bnxgyuYCkC1ugAdyvJ2FCv7xUppc7PvyIQ1gQS/nOP0wKcZiFiqxpVBoVKzSv/Tw4ct8p/WL2u75xakZj5oM6ztTdwYwxnRcs5EylWZ1QD7m/y9pwLO79arvbZggQff1GkvbJ3FM/arlsE2st4NZ3HIHFmU/3Nn9PsBb5uiogYN08coGZaspMlD4YbNSo4bOu5hDmwzdOdTwC9vg4Xfq7IAMHADhs/3ji0pI1IRPsELc3RR6tGhDAgMBAAECggEAQal4VjcxKQ6n4kjwrFWNdzCmhgATmHf3rGAW9zKBdqFknZkvb6yKOiIWKcs4FBHc2VEePG0xxAV+Tm2iE4dclciq7tU8R+N5RIO1mBqIC9p8a1LQ+bUF2X6fWdTpGC19Riq1ejQkmPWaEDeUp8m3u8UOoGUiQppE++R1bjBZNaT5S16qbfDOV9plF550wnwbq6fNZlWT4PdiI6ox/4KPZdhIKGKnKkh4xX4mHk3E9fl+udHbXiT3qjSDOEchUpHglzNZG1LMD2BWb+zcxUbJzm2r5BviZmKHPd4+w5mt+kfPbDHnFCgjnlZoFswNFO2s/ZHk99NveoDa1i0OVGbuwQKBgQD045x067oKAvBcRawtP5H6DaifBFnwp3xTg8GIPwitD8+bQMIi6s0jbN7HI7A7S5BTFnukCweURhBiXmbrs29ImiaxcIVGdBKDhkHABq3oino4oHVs8bLpD9moaQccon79aPQ38j8KeU1UJ7a4R6Jbd2eLoZPmj5bPrQHkbqDWWwKBgQDeZDjS0tLtOjbY9CHdB9+BWSyL2DylqSyec/Ew/c8sr2SBK59Db0W2Vgc62iTOYjzWYBTBUrYWRRoAnSoQLkePjQ+mpzGMtpR9BKq3ADrIremgJGRFIo+NL9qjpJu238na+FGp1DgfTSXMMxzLC23lTgh6PXIIcF03kL/yN9lqOQKBgQDPCmCMuX9gV3u/h2g6GTThpAqb5qHjxLZoJUzKVACR0HxFVkrMGpe1C6aN1q54czpiBPAjkO+nfFT91bJONDYxu6JbAjarihbc+/U61GrT37/VgFPG99G7GZt7ttA8dWXH+aQAaN7DjCrEq47f3jB2BE2Wz9SraVqn2i1vY9i3YQKBgQCu0d4RbIU+0upWteMA27WI+s6XyA40s75NeRr6xipcGCxLlj0GR6xnX00jqGQSkQr+Al2OczSMYRnFrcZpHdhHMj5BZWEAGm6zsD16ygVrx7rFlpXz+u0ZsaqPxVBa+6S0K0wW0qqjgIPb97oEqyFihmsHnNHNbHb6vSEGiXyxkQKBgAwb/3lWqp1Zpj6hMw9NdB0c6huQYLqX2INkKj9PcIlFq0nOeHMZfMisuQKhvcGsPQsHMP2NbPjZiLnbpRHPvplU0p7ayaXuNF2t73k/L5f92+8VBuYECEUOXw2xST5gvkPdKGK1xM1cLT6y8TrFRIXvUK2duHjDxiaPKtANi2P4`)
+	publicKeyInline  = []byte(`MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1L1A8fiz0NEvi/uVJrQ9hCcNS/ed5lQjFgSZLQW6j0yIQ5q3d9/t3DatLe617gC4aKmUv+d5w3IVNcLAr3onjQ0e0rHTuBzEuG58YMrmApAtboAHcrydhQr+8VKaXOz78iENYEEv5zj9MCnGYhYqsaVQaFSs0r/08OHLfKf1i9ru+cWpGY+aDOs7U3cGMMZ0XLORMpVmdUA+5v8vacCzu/Wq722YIEH39RpL2ydxTP2q5bBNrLeDWdxyBxZlP9zZ/T7AW+boqIGDdPHKBmWrKTJQ+GGzUqOGzruYQ5sM3TnU8Avb4OF36uyADBwA4bP944tKSNSET7BC3N0UerRoQwIDAQAB`)
 )
 
 func TestRSAImplEncrypt(t *testing.T) {
 	encodedString, err := RSA().Encrypt(rawString, publicKey)
+	t.Log(encodedString)
+	assert.NoError(t, err)
+
+	//inline
+	encodedString, err = RSA().Encrypt(rawString, publicKeyInline)
 	t.Log(encodedString)
 	assert.NoError(t, err)
 }
@@ -61,10 +69,24 @@ func TestRSAImplDecrypt(t *testing.T) {
 	t.Log(decodedString)
 	assert.NoError(t, err)
 	assert.Equal(t, decodedString, rawString)
+
+	//inline
+	encodedString, err = RSA().Encrypt(rawString, publicKeyInline)
+	t.Log(encodedString)
+	assert.NoError(t, err)
+	decodedString, err = RSA().Decrypt(encodedString, privateKeyInline)
+	t.Log(decodedString)
+	assert.NoError(t, err)
+	assert.Equal(t, decodedString, rawString)
 }
 
 func TestRSAImplSign(t *testing.T) {
 	sign, err := RSA().Sign([]byte(rawString), privateKey)
+	t.Log(Base64().Encode([]byte(sign)))
+	assert.NoError(t, err)
+
+	//inline
+	sign, err = RSA().Sign([]byte(rawString), privateKeyInline)
 	t.Log(Base64().Encode([]byte(sign)))
 	assert.NoError(t, err)
 }
@@ -75,6 +97,15 @@ func TestRSAImplVerifySign(t *testing.T) {
 	assert.NoError(t, err)
 
 	ok, err := RSA().VerifySign([]byte(rawString), []byte(sign), publicKey)
+	assert.NoError(t, err)
+	assert.Equal(t, true, ok)
+
+	//inline
+	sign, err = RSA().Sign([]byte(rawString), privateKeyInline)
+	t.Log(sign)
+	assert.NoError(t, err)
+
+	ok, err = RSA().VerifySign([]byte(rawString), []byte(sign), publicKeyInline)
 	assert.NoError(t, err)
 	assert.Equal(t, true, ok)
 }
