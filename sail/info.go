66fcd0856cb02f5f52be9b8ced33d4b9723a7d26
keepchen <keepchen2016@qq.com>
2025-04-03 17:15:09 +0800

    printinfo代码优化

--- sail/info.go
+++ sail/info.go
@@ -20,42 +20,42 @@ func printSummaryInfo(conf config.HttpServerConf, ginEngine *gin.Engine) {
 		protocol     = "http:"
 		messages     bytes.Buffer
 		localIp, _   = utils.IP().GetLocal()
-		delimiter    = []byte(strings.Repeat("=", 88))
-		subDelimiter = []byte(strings.Repeat("-", 88))
+		delimiter    = strings.Repeat("=", 88)
+		subDelimiter = strings.Repeat("-", 88)
 		repoLink     = "Repository: https://github.com/keepchen/go-sail"
 		blank        = strings.Repeat(" ", len(delimiter)-len(repoLink)-len(constants.GoSailVersion)-11)
 	)
-	messages.Write(delimiter)
+	messages.WriteString(delimiter)
 	info := fmt.Sprintf("%s\n", constants.GoSailLogo)
-	messages.Write([]byte(info))
+	messages.WriteString(info)
 	versionInfo := fmt.Sprintf("\n%s%s(version: %s)\n", repoLink, blank, constants.GoSailVersion)
-	messages.Write([]byte(versionInfo))
-	messages.Write(subDelimiter)
-	messages.Write([]byte("\n"))
+	messages.WriteString(versionInfo)
+	messages.WriteString(subDelimiter)
+	messages.WriteString("\n")
 	sMgs := fmt.Sprintf("[Server] Listening at: {%s}\n", conf.Addr)
-	messages.Write([]byte(sMgs))
+	messages.WriteString(sMgs)
 	if conf.Debug {
 		ginEngine.GET("/go-sail", func(c *gin.Context) {
 			c.String(http.StatusOK, fmt.Sprintf("%s\r\n\r\n/** This route only enabled in debug mode **/", constants.GoSailLogo))
 		})
 		msg := fmt.Sprintf(">\t%s//%s%s%s\n", protocol, localIp, conf.Addr, "/go-sail")
-		messages.Write([]byte(msg))
+		messages.WriteString(msg)
 	}
 	if conf.Swagger.Enable {
 		msg := fmt.Sprintf("[Swagger] Enabled:\n>\t%s//%s%s%s     (Redocly UI)\n>\t%s//%s%s%s  (Swagger UI)\n",
 			protocol, localIp, conf.Addr, "/redoc/docs.html",
 			protocol, localIp, conf.Addr, "/swagger/index.html")
-		messages.Write([]byte(msg))
+		messages.WriteString(msg)
 	}
 	if conf.Prometheus.Enable {
 		msg := fmt.Sprintf("[Prometheus] Enabled:\n>\t%s//%s%s%s\n", protocol, localIp, conf.Prometheus.Addr, conf.Prometheus.AccessPath)
-		messages.Write([]byte(msg))
+		messages.WriteString(msg)
 	}
 	if conf.Debug {
 		msg := fmt.Sprintf("[Pprof] Enabled:\n>\t%s//%s%s%s\n", protocol, localIp, conf.Addr, "/debug/pprof")
-		messages.Write([]byte(msg))
+		messages.WriteString(msg)
 	}
-	messages.Write(delimiter)
+	messages.WriteString(delimiter)
 
 	fmt.Println(messages.String())
 }
