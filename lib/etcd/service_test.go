ef76eeb27545d56db1e983c8e4042606625ae9c3
keepchen <keepchen2016@qq.com>
2025-05-08 11:08:58 +0800

    other:继续完善测试用例

--- lib/etcd/service_test.go
+++ lib/etcd/service_test.go
@@ -3,44 +3,52 @@ package etcd
 import (
 	"context"
 	"fmt"
+	"net"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestRegisterService(t *testing.T) {
 	t.Run("RegisterService", func(t *testing.T) {
-		assert.Panics(t, func() {
-			Init(conf)
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-			defer cancel()
-			t.Log(RegisterService(ctx, "go-sail", "endpoint", 60))
-		})
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s", conf.Endpoints[0]))
+		if err != nil {
+			return
+		}
+		_ = conn.Close()
+		Init(conf)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		t.Log(RegisterService(ctx, "go-sail", "endpoint", 60))
 	})
 }
 
 func TestDiscoverService(t *testing.T) {
 	t.Run("DiscoverService", func(t *testing.T) {
-		assert.Panics(t, func() {
-			Init(conf)
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-			defer cancel()
-			t.Log(DiscoverService(ctx, "go-sail"))
-		})
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s", conf.Endpoints[0]))
+		if err != nil {
+			return
+		}
+		_ = conn.Close()
+		Init(conf)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		t.Log(DiscoverService(ctx, "go-sail"))
 	})
 }
 
 func TestWatchService(t *testing.T) {
 	t.Run("WatchService", func(t *testing.T) {
-		assert.Panics(t, func() {
-			Init(conf)
-			fn := func(k, v []byte) {
-				fmt.Println(k, v)
-			}
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-			defer cancel()
-			WatchService(ctx, "go-sail", fn)
-		})
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s", conf.Endpoints[0]))
+		if err != nil {
+			return
+		}
+		_ = conn.Close()
+		Init(conf)
+		fn := func(k, v []byte) {
+			fmt.Println(k, v)
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		WatchService(ctx, "go-sail", fn)
 	})
 }
--- lib/etcd/watch_test.go
+++ lib/etcd/watch_test.go
@@ -3,39 +3,58 @@ package etcd
 import (
 	"context"
 	"fmt"
-	"github.com/stretchr/testify/assert"
+	"net"
 	"testing"
+	"time"
 )
 
 func TestWatch(t *testing.T) {
 	t.Run("Watch", func(t *testing.T) {
-		assert.Panics(t, func() {
-			fn := func(k, v []byte) {
-				fmt.Println(k, v)
-			}
-			Watch(context.Background(), "go-sail", fn)
-		})
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s", conf.Endpoints[0]))
+		if err != nil {
+			return
+		}
+		_ = conn.Close()
+		fn := func(k, v []byte) {
+			fmt.Println(k, v)
+		}
+		Init(conf)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		Watch(ctx, "go-sail", fn)
 	})
 }
 
 func TestWatchWithPrefix(t *testing.T) {
 	t.Run("WatchWithPrefix", func(t *testing.T) {
-		assert.Panics(t, func() {
-			fn := func(k, v []byte) {
-				fmt.Println(k, v)
-			}
-			WatchWithPrefix(context.Background(), "go-sail", fn)
-		})
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s", conf.Endpoints[0]))
+		if err != nil {
+			return
+		}
+		_ = conn.Close()
+		fn := func(k, v []byte) {
+			fmt.Println(k, v)
+		}
+		Init(conf)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		WatchWithPrefix(ctx, "go-sail", fn)
 	})
 }
 
 func TestWatchWith(t *testing.T) {
 	t.Run("WatchWith", func(t *testing.T) {
-		assert.Panics(t, func() {
-			fn := func(k, v []byte) {
-				fmt.Println(k, v)
-			}
-			WatchWith(context.Background(), "go-sail", fn)
-		})
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s", conf.Endpoints[0]))
+		if err != nil {
+			return
+		}
+		_ = conn.Close()
+		fn := func(k, v []byte) {
+			fmt.Println(k, v)
+		}
+		Init(conf)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		WatchWith(ctx, "go-sail", fn)
 	})
 }
